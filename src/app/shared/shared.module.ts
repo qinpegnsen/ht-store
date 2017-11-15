b4751f8d8187e7605dc044ae30106c5c95ab02f4
gaojie <1226898330@qq.com>
2017-11-15 12:12:07 +0800

    up shareModule导入依赖模块

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,22 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NgZorroAntdModule} from "ng-zorro-antd";
 import {RouterModule} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,                 //核心模块，必须
+    FormsModule,                  //表单支持
+    ReactiveFormsModule,          //表单支持
     NgZorroAntdModule.forRoot()   //zorroUI库
   ],
   declarations: [],
   providers: [],
   exports: [
+    CommonModule,           //核心模块，必须
     RouterModule,           //路由依赖模块
+    FormsModule,            //表单支持
+    ReactiveFormsModule,    //表单支持
     NgZorroAntdModule       //zorroUI库
   ]
 })
