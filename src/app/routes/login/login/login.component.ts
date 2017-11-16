a13750608838f766efcfc405500f364e5b05388c
likun <koupengxiang@163.com>
2017-11-16 12:00:44 +0800

    up 完善登陆页面

--- src/app/routes/login/login/login.component.ts
+++ src/app/routes/login/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Setting} from "../../../public/setting/setting";
 
@@ -8,18 +8,19 @@ import {Setting} from "../../../public/setting/setting";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  array = [ '../../../assets/img/bak/1.jpg' ];//广告banner
+  array = ['../../../assets/img/bak/1.png'];//广告banner
   validateForm: FormGroup;//登录的表单
   app = Setting.APP; //平台基本信息
 
   //用于登录时的表单
   _submitForm() {
     for (const i in this.validateForm.controls) {
-      this.validateForm.controls[ i ].markAsDirty();
+      this.validateForm.controls[i].markAsDirty();
     }
   }
 
-  constructor(public fb: FormBuilder) { }
+  constructor(public fb: FormBuilder) {
+  }
 
   ngOnInit() {
     let _this = this;
@@ -27,18 +28,17 @@ export class LoginComponent implements OnInit {
      *广告banner定时器
      */
     setTimeout(_ => {
-      _this.array = [ '../../../assets/img/bak/1.jpg', '../../../assets/img/bak/2.png' ];
-    }, 500);
-
+      _this.array = ['../../../assets/img/bak/1.png', '../../../assets/img/bak/2.png'];
+    }, 5000);
 
     /**
      * 用于登录时的表单验证
      * @type {FormGroup}
      */
     _this.validateForm = this.fb.group({
-      userName: [ null, [ Validators.required ] ],
-      password: [ null, [ Validators.required ] ],
-      remember: [ true ],
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true],
     });
   }
 
