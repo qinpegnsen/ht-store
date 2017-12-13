1fe7677f75133b967aa7a4ca05ca7833e0dcc431
xiejiaojiao <2583206346@qq.com>
2017-12-13 09:50:35 +0800

    up 企业店铺信息编码修改

--- src/app/routes/store-base/store-info/store-info.component.ts
+++ src/app/routes/store-base/store-info/store-info.component.ts
@@ -15,11 +15,14 @@ export class StoreInfoComponent implements OnInit {
   public enumState: any = Setting.ENUMSTATE;//获取枚举状态名
   public enum: any = Setting.ENUM;//获取枚举状态名
   public shops: string = SettingUrl.ROUTERLINK.basic.shops; //店铺信息路由
+  public epCode: any;//代理商编码
   constructor(public location: Location) {
   }
 
   ngOnInit() {
     let me = this;
+    let collection = JSON.parse(localStorage.getItem('loginInfo'));//获取企业的编码
+    me.epCode = collection.data.epCode;//获取企业的编码
     me.queryStoreData();//查询企业信息
     me.bankCard();//银行卡号加密
     me.idCard();//身份证号加密
@@ -32,7 +35,7 @@ export class StoreInfoComponent implements OnInit {
     let me = this;
     me._loading = true; //锁屏
     let data = { //查询参数
-      epCode: "649255483008294912"//企业编码
+      epCode: me.epCode//企业编码
     }
     $.when(StoreBaseService.loadStoreInfo(data)).done(data => {
       me._loading = false //解除锁屏
@@ -47,7 +50,7 @@ export class StoreInfoComponent implements OnInit {
    * 返回上一页
    */
   back() {
-    let me=this;
+    let me = this;
     me.location.back();
   }
 
