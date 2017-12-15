4d1e631c355697bf650a48aeb04f12318ca659da
yanweiyi <m15901019392_1@163.com>
2017-12-15 11:38:53 +0800

    添加简易页面的头部退出登录

--- src/app/layout/simple/simple.component.ts
+++ src/app/layout/simple/simple.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {Setting} from "../../public/setting/setting";
+import {AjaxService} from "../../public/service/ajax.service";
+import {SettingUrl} from "../../public/setting/setting_url";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-simple',
@@ -10,9 +13,24 @@ import {Setting} from "../../public/setting/setting";
 export class SimpleComponent implements OnInit {
   public app = Setting.APP; //平台信息
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit() {
   }
 
+  /**
+   * 退出登录
+   */
+  logout() {
+    localStorage.clear(); //清空所有storage
+    //执行查询（异步）
+    AjaxService.get({
+      url: SettingUrl.URL.login.logout,
+      success: (result) => {
+        if (result.success) {
+          this.router.navigate([SettingUrl.ROUTERLINK.pass.login])//跳到登录页面
+        }
+      }
+    });
+  }
 }
